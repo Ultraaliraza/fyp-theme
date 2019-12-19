37cb538e4b468322d6d06778ddd71b4d7d727282
Ali Raza <55051241+Ultraaliraza@users.noreply.github.com>
2019-12-18 19:53:21 -0800

    ali
    sa

--- src/app/service/auth-service/authentication.service.ts
+++ src/app/service/auth-service/authentication.service.ts
@@ -12,8 +12,8 @@ export class AuthenticationService {
     throw new Error("Method not implemented.");
   }
   keyvalue = '';
-   apiHeader = 'http://localhost:4000/';
-  // apiHeader = 'https://us-central1-helpinghand-90a6a.cloudfunctions.net/apis/';
+  //  apiHeader = 'http://localhost:4000/';
+  apiHeader = 'https://us-central1-helpinghand-90a6a.cloudfunctions.net/apis/';
   // apiHeader = 'http://localhost:5000/helpinghand-90a6a/us-central1/apis/';
 
   user = new BehaviorSubject({});
